2baf7d54958b06956d3ad667621a444a2bdc3def
Tanjina sultana <tanjinasultana388@gmail.com>
2023-09-02 12:04:32 +0600

    button added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const buttonCategory = async () => {
+    const res = await fetch(" https://openapi.programming-hero.com/api/videos/categories");
+    const data = await res.json();
+    console.log(data.data[0].category)
+    const buttonContainer =document.getElementById("button-container");
+    data.data.forEach(category => {
+        console.log(category.category)
+        const button = document.createElement("button");
+        button.innerHTML = `<button class="btn normal-case btn-active">${category.category}</button>`;
+        buttonContainer.appendChild(button);
+       
+    });
+}
+buttonCategory();
